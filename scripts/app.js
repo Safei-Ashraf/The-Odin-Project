4f76b0d86203f89ae259bf7cc90ca3140242e365
Safei Ashraf <Safei.ashraf@gmail.com>
2020-10-26 19:11:26 +0200

    adding reset and build buttons logic

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,20 @@
-//create 16 * 16 grid:
+//global vars DOM:
 const mainElement = document.querySelector('main');
 const container = document.querySelector('.container');
-//for height we gonna create a new div with class container
-//for width we gonna create a new div with class grid-cell
-makeGrid(5,5);
-function makeGrid(height, width)
+const makeGridBtn = document.querySelector('#submit-btn');
+const gridCells = document.querySelectorAll('.grid-cell');
+const resetBtn = document.querySelector('#reset-btn');
+
+
+//to build grid:
+makeGridBtn.addEventListener('click', ()=>{
+    mainElement.innerHTML = ''; 
+    const gridSize = document.querySelector('input');
+    let inputValue = parseInt(gridSize.value);
+    makeGrid(inputValue,inputValue);    
+});
+
+function makeGrid(height = 20, width = 20)
 {
     for(let i = 1; i <= height; i++)
         {
@@ -24,8 +34,6 @@ function fillRow(row,width){
         }
 }
 //Event on each cell of the grid on hover to color it randomly:
-const gridCells = document.querySelectorAll('.grid-cell');
-
 gridCells.forEach(element => {
     element.addEventListener('mouseover',(e)=>{
         let redRatio = greenRatio = blueRatio = 255;
@@ -35,7 +43,11 @@ gridCells.forEach(element => {
         e.target.style.background = `rgb(${redRatio},${greenRatio},${blueRatio})`
     });
 });
-
+//Reset Grid:
+resetBtn.addEventListener('click',function resetGrid(e){
+    e.preventDefault();
+    mainElement.innerHTML = '';  
+})
 //Helper method:
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
