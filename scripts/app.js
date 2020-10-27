a85d782f0f329ebb79eb5ef44dcb5c72be342363
Safei Ashraf <Safei.ashraf@gmail.com>
2020-10-27 18:54:46 +0200

    adding basic files

--- scripts/app.js
+++ scripts/app.js
@@ -1,69 +1,34 @@
-//global vars DOM:
-const mainElement = document.querySelector('main');
-const makeGridBtn = document.querySelector('#submit-btn');
-const resetBtn = document.querySelector('#reset-btn');
-const container = document.querySelector('.container');
-
-
-//to build grid:
-
-makeGridBtn.addEventListener('click', ()=>{
-    mainElement.innerHTML = ''; 
-    const gridSize = document.querySelector('input');
-    let inputValue = parseInt(gridSize.value);
-
-    if(inputValue)
-    {
-    if(inputValue > 60){
-        alert('Hey Hey, I said max value is 60 (-_-;) Now it will be a ZERO for you');
-        
-    }
-    else{
-        makeGrid(inputValue,inputValue);  
-        //coloring process start after input, so the grid already exisit to use on DOM:
-        const gridCells = document.querySelectorAll('.grid-cell');
-        gridCells.forEach(element => {
-        element.addEventListener('mouseover',(e)=>{
-            let redRatio = greenRatio = blueRatio = 255;
-            redRatio = getRandomInt(255);
-            blueRatio = getRandomInt(255);
-            greenRatio = getRandomInt(255);
-            e.target.style.background = `rgb(${redRatio},${greenRatio},${blueRatio})`
-    });
-});
-
-    }
-    }else{
-        alert('Please stop drinking and add a size to start with (-_-;)')
-        }
-});
-
-function makeGrid(height, width)
-{
-    for(let i = 1; i <= height; i++)
-        {
-            const row = document.createElement('div');
-            row.classList.add('container');
-            mainElement.appendChild(row);
-            fillRow(row,width);
-        }
+//Calcaulator logic:
+function add(x,y) {
+    return x+y;
 }
-
-function fillRow(row,width){
-    for(let i = 1; i <= width; i++)
-        {
-            const column = document.createElement('div');
-            column.classList.add('grid-cell');
-            row.appendChild(column);
-        }
+function subtract(x,y) {
+    return x-y;
 }
-
-//Reset Grid:
-resetBtn.addEventListener('click',function resetGrid(e){
-    e.preventDefault();
-    mainElement.innerHTML = '';  
-})
-//Helper method:
-function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+function multiply(x,y) {
+    return x-y;
+}
+function divide(x,y) {
+    return x/y;
+}
+function operate (operator,x,y) {
+    switch (operator) {
+        case `-`:
+            console.log('deduction');
+            return x - y;
+            break;
+        case `+`:
+            console.log('add');
+            return x + y;
+            break;
+        case `*`:
+            console.log('***');
+            return x * y;
+            break;
+        case `/`:
+            console.log('division');
+            return x / y;
+            break;
+    
+    }
 }
